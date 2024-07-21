da8bad43d9321bbea746cd528e5da33e4bbd4b95
AlexShyshko <shish1988@mail.ru>
2024-07-21 22:34:58 +0300

    FEAT: husky added

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,44 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/stylistic",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:prettier/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    // tsconfigRootDir: __dirname,
+    // project: true,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: [
+    "react-refresh",
+    "react",
+    "@typescript-eslint",
+    "eslint-plugin-react-compiler",
+  ],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "@typescript-eslint/no-explicit-any": "error",
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+    "react/jsx-no-target-blank": "off",
+    "react-compiler/react-compiler": "error",
   },
-}
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
---
+++ src/.prettierrc.js
@@ -0,0 +1,16 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+    parser: "typescript",
+    trailingComma: "all",
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    useTabs: true,
+    experimentalTernaries: true,
+    bracketSpacing: true,
+  };
+  
+  export default config;
